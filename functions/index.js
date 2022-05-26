a4d7a5af638e8f8613e7bb6bb0247d79e4b98de2
Todd Albert <todd@bocacode.com>
2022-05-26 16:12:45 -0400

    added rating ability

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ import functions from 'firebase-functions';
 import express from 'express';
 import cors from 'cors';
 import { getAllRestaurants, getRestaurantById, deleteRestaurant,
-  updateRestaurant, addRestaurant } from './src/restaurants.js';
+  updateRestaurant, addRestaurant, updateRestaurantRating } from './src/restaurants.js';
 
 const app = express();
 app.use(cors());
@@ -13,6 +13,7 @@ app.get('/restaurants', getAllRestaurants);
 app.get('/restaurants/:restaurantId', getRestaurantById);
 app.delete('/restaurants/:restaurantId', deleteRestaurant);
 app.patch('/restaurants/:restaurantId', updateRestaurant);
+app.patch('/restaurants/:restaurantId/rating', updateRestaurantRating);
 app.post('/restaurants', addRestaurant);
 
 export const api = functions.https.onRequest(app);
--- functions/src/restaurants.js
+++ functions/src/restaurants.js
@@ -84,3 +84,31 @@ export function deleteRestaurant(req, res) {
       res.status(500).send(err);
     });
 }
+
+export function updateRestaurantRating(req, res) {
+  const { restaurantId } = req.params;
+  if(!req.body || !req.body.rating || req.body.rating > 5 || req.body.rating < 0) {
+    // problem
+    res.status(401).send('Invalid request');
+    return;
+  }
+  const newRating = req.body.rating;
+  const db = connectDb();
+  // get the restaurant (hitting Firestore)
+  db.collection('restaurants').doc(restaurantId).get()
+    .then(doc => {
+      const { ratingList } = doc.data();
+      // do maths (doing JS)
+      const newRatingList = (ratingList) ? [...ratingList, newRating] : [newRating];
+      const numRatings = newRatingList.length;
+      const rating = newRatingList.reduce((accum, elem) => accum + elem, 0) / numRatings;
+      const updatedData = { ratingList: newRatingList, numRatings, rating };
+      // save restaurant (hitting Firestore)
+      db.collection('restaurants').doc(restaurantId).update(updatedData)
+        .then(() => getRestaurantById(req, res))
+    })
+    .catch(err => {
+      res.status(500).send(err);
+      return;
+    })
+}
